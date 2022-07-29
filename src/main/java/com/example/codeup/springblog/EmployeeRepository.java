2a6e79b5ce5a841d63755eee9a424c21119e743e
“Cory <coryurton@gmail.com>
2022-07-29 15:51:16 -0500

    feat: failed experiment

---
+++ src/main/java/com/example/codeup/springblog/EmployeeRepository.java
@@ -0,0 +1,12 @@
+//package com.example.codeup.springblog;
+//
+//
+//import org.springframework.data.repository.CrudRepository;
+//
+//public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+//
+//
+//
+//}
+//
+//
--- src/main/java/com/example/codeup/springblog/SpringblogApplication.java
+++ src/main/java/com/example/codeup/springblog/SpringblogApplication.java
@@ -1,7 +1,9 @@
 package com.example.codeup.springblog;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringblogApplication {
@@ -9,5 +11,22 @@ public class SpringblogApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringblogApplication.class, args);
     }
+
+//
+//    @Bean
+//    public CommandLineRunner run(EmployeeRepository repository) {
+//        return (args -> {
+//            insertJavaAdvocates(repository);
+//            System.out.println(repository.findAll());
+//        });
+//    }
+//
+//    public void insertJavaAdvocates(EmployeeRepository repository) {
+//        repository.save(new Employee("Mister", "Advocate"));
+//        repository.save(new Employee("Missy", "Advocate"));
+//        repository.save(new Employee("Senor", "Advocate"));
+//        repository.save(new Employee("Senorita", "Proponente"));
+//        repository.save(new Employee("Erb", "Jean"));
+//    }
 
 }
